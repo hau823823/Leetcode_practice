8b333b0385a90a380680226568f480887c2554ba
hau <hau823823@gmail.com>
2024-09-22 00:25:58 +0800

    finish longestCommonSubsequence

--- longestCommonSubsequence/longestCommonSubsequence.go
+++ longestCommonSubsequence/longestCommonSubsequence.go
@@ -1,23 +1,33 @@
 package longestCommonSubsequence
 
+// 2-D dynamic
+// Bottom Up
 func LongestCommonSubsequence(text1 string, text2 string) int {
 
 	n, m := len(text1), len(text2)
-	dp := make([][]int, n)
+	dp := make([][]int, n + 1)
 
-	for i := 0; i < n; i++ {
-		dp[i] = make([]int, m)
-		dp[i][0] = 0
-	}
-	for j := 0; j < m; j++ {
-		dp[0][j] =0
+	for i := 0; i < n + 1; i++ {
+		dp[i] = make([]int, m + 1)
 	}
 
-	for i := 1; i < n; i++ {
-		for j := 1; j < m; j++ {
-			dp[i][j] = dp[i-1][j-1] + 1
+	for i := n-1; i >= 0; i-- {
+		for j := m-1; j >= 0; j-- {
+			if text1[i] == text2[j] {
+				dp[i][j] = dp[i+1][j+1] + 1
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+			}
 		}
 	}
 
-    return 0
+
+    return dp[0][0]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
