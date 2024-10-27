f2ab206c0c667f105e2219b205dff2c4abcba072
hau <hau823823@gmail.com>
2024-10-27 15:50:51 +0800

    target sum brute force solve

--- targetSum/targetSum.go
+++ targetSum/targetSum.go
@@ -1,5 +1,23 @@
 package targetSum
 
+// brute force
 func FindTargetSumWays(nums []int, target int) int {
-	return 0
+	count := 0
+
+	var backtrack func(int, int)
+	backtrack = func(index, sum int) {
+		if index == len(nums) {
+			if sum == target {
+				count++
+			}
+			return
+		}
+		backtrack(index+1, sum-nums[index])
+		backtrack(index+1, sum+nums[index])
+	}
+
+	backtrack(0, 0)
+	return count
 }
+
+// memorized
